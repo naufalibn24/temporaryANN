b5cfb74389aa0a95d1716a748da87e03d7ee5c3e
bloodybuttock <jurukuncicpm@gmail.com>
2021-01-03 00:33:41 +0700

    herokuuu 3

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import routes from "../src/routes";
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT =  5000;
 
 mongooseconnect();
 
@@ -23,5 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(PORT, () => console.log(`server running on localhost://${PORT}`));
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    "Express server listening on port %d in %s mode"
+    // this.address().port,
+    // app.settings.env
+  );
+});
+// app.listen(PORT, () => console.log(`server running on localhost://${PORT}`));
 // taskkill /F /IM node.exe
