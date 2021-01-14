ddffb12a4a767521455c7bb3a41e67cd5361fd3d
bloodybuttock <jurukuncicpm@gmail.com>
2021-01-14 11:01:46 +0700

    wdwdw

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,8 @@ const PORT: string | number = process.env.PORT || 5000;
 mongooseconnect();
 
 var corsOptions = {
-  origin: "http://localhost:4200",
+  origin: "*",
+  // origin: "http://localhost:4200",
   // origin: "https://revision-ass4-git-main.muhidabdul168.vercel.app",
   credentials: true,
 };
